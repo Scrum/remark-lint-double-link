f27d1ed162f36056479f8d4f81daf2507b3cdc3d
Ivan Demidov <Scrum@list.ru>
2020-07-20 10:27:50 +0300

    test: normalize url, for issue #2

--- test/test.js
+++ test/test.js
@@ -8,7 +8,8 @@ const processor = remark().use(lint).use(doubleLink);
 const nok = `![img](./logo)
 [super-link-1](http://link-1)
 - [puper-link-1](http://link-1)
-- [puper-link-1](http://link-2)`;
+- [puper-link-1](http://link-2)
+- [puper-link-1](http://link-2/index.php?foo=bar&ref=test_ref#content)`;
 
 const ok = `- [puper-link-1](http://link-1)
 - [puper-link-1](http://link-2)`;
@@ -26,7 +27,9 @@ test('remark-lint-double-link invalid', t => {
     processor.processSync(nok).messages.map(String),
     [
       '2:1-2:30: http://link-1',
-      '3:3-3:32: http://link-1'
+      '3:3-3:32: http://link-1',
+      '4:3-4:32: http://link-2',
+      '5:3-5:71: http://link-2/index.php?foo=bar&ref=test_ref#content'
     ],
     'should work on valid fixtures'
   );
